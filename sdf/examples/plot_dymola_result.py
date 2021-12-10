08c10c11977dcf6e7644b1a81b4c35a2a1b98161
Torsten Sommer <torsten.sommer@3ds.com>
2021-12-10 13:50:48 +0100

    Add Dymola result file example

---
+++ sdf/examples/plot_dymola_result.py
@@ -0,0 +1,27 @@
+import sdf
+from sdf.plot.plot_time_series import plot_time_series
+import matplotlib.pyplot as plt
+
+
+# the Dymola result file
+filename = 'IntegerNetwork1.mat'
+
+# model variables to plot
+variables = ['sum.y', 'product.y', 'triggeredAdd.y', 'multiSwitch1.y']
+
+datasets = []
+
+for variable in variables:
+    # convert the Modelica path to an absolute SDF/HDF5 path
+    path = '/' + variable.replace('.', '/')
+    # read the dataset
+    datasets.append(sdf.load(filename, path))
+
+# plot the datasets
+figure, ax = plt.subplots()
+
+for dataset, variable in zip(datasets, variables):
+    ax.plot(dataset.scales[0].data, dataset.data, label=variable)
+
+ax.legend()
+plt.show()
