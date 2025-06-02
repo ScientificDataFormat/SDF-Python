89f1928875068cef3fb3615edd6c63b567216e76
Torsten Sommer <torsten.sommer@3ds.com>
2025-06-02 13:35:03 +0200

    Use pyproject.toml and CMakeLists.txt

--- build_mac.sh
+++
@@ -1,8 +0,0 @@
-echo Build the shared library
-clang -dynamiclib C/src/Python.c C/NDTable/src/Core.c C/NDTable/src/Interpolation.c -IC/include -IC/NDTable/include -o sdf/darwin64/libNDTable.dylib
-
-echo List exported symbols
-nm -gU sdf/darwin64/libNDTable.dylib
-
-echo Build the distribution archive
-python setup.py sdist
